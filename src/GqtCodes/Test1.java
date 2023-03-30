1e7c6f6996396ca691ebd430813b5cc98b53cb77
Lenovo <Lenovo@DESKTOP-UVGG9EI>
2023-03-29 20:41:50 -0700

    HII

--- src/GqtCodes/Test1.java
+++ src/GqtCodes/Test1.java
@@ -1,11 +1,101 @@
 package GqtCodes;
 
+import java .util.Scanner;
+
 public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-
+		Scanner sc = new Scanner(System.in);	
+		System.out.println("Enter the value:");
+		int n = sc.nextInt();
+		for(int i=0; i<=n-1; i++) {
+			for(int j=0; j<=n-1; j++) {
+				if(i==0 && j<n/2 || i==n-1 && j<n/2 || j==n/4) {
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for(int j=0; j<=n-1; j++) {
+				if(j==0 || i==n-1) {
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for(int j=0; j<=n-1; j++) {
+				if(j==0 && i!=0 && i!=n-1 || i==n-1 && j<n/2 && j!=0 || j==n/2 && i!=0 && i!=n-1 || i==0 && j!=0 && j<n/2) {
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for(int j=0; j<=n-1; j++) {
+				if(i-j==n/2 || i+j==(n-1)+n/2 ) {
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for(int j=0; j<=n-1; j++) {
+				if(j==0 || i==0 && j<n/2 || i==n-1 && j<n/2 || i==n/2 && j<n/2) {
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for(int j=0; j<=n-1; j++) {
+				if(j==0 || i==j && j<=n/2 || i+j==n-1 && j>=n/2 || j==n-1) {
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for(int j=0; j<=n-1; j++) {
+				if(i==j && j<n/2 || i+j==n-1) {
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for(int j=0; j<=n-1; j++) {
+				if(i==0 && j!=0 && j<n/2 || j==0 && i<n/2 && i!=0 || i==n/2 && j<n/2 || j==n/2 && i>n/2 && i!=n-1 || i==n-1 && j<n/2) {
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for(int j=0; j<=n-1; j++) {
+				if(j==0 || j==n/2 || i==n/2 && j<=n/2 || i==0 && j<=n/2) {
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for(int j=0; j<=n-1; j++) {
+				if(i==0 && j<n/2 || i==n-1 && j<n/2 || j==n/4) {
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+			}
+			}
 	}
 
-}
+	
+
+	
+
---
+++ src/com/gqt/arrayysproj/package1/ArraryCode1.java
@@ -0,0 +1,13 @@
+package com.gqt.arrayysproj.package1;
+
+import java.util.Scanner;
+
+public class ArraryCode1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+
+	}
+
+}
